d30099894fcbda4d019b2ddb4ba81189eb397462
Faisal-Hussein <fh069017@ohio.edu>
2019-09-06 17:15:45 -0400

    git lab program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Faisal Hussein
+ *        Date: 9/6/2019
+ * Description: Lab 2, git lab program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
